bf1541e5f9b84a000f3093eb31aa62ac65ac2035
editmatadorbot <32656974+editmatadorbot@users.noreply.github.com>
2017-10-10 04:34:28 +0330

    bot.lua

--- cli/ bot/bot.lua
+++ cli/ bot/bot.lua
@@ -92,28 +92,10 @@ function create_config( )
     "Tools",
     "Write"
 	},
-    sudo_users = {377450049,418516842,284298227},
+    sudo_users = {377450049,418516842},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[
-	》MaTaDoR BoT v5.7
-An advanced administration bot based on https://valtman.name/telegram-cli
-
-》https://github.com/BeyondTeam/BDReborn 
-
-》Admins :
-》@MahDiRoO ➣ Founder & Developer《
-》@JavadSudo ➣ Developer《
-》@Shaniloop ➣ Developer《
-
-》Special thanks to :
-》MaTaDoRTeaM
-》@Xamarin_Devloper
-
-》Our channel :
-》@MaTaDoRTeam《
-]],
   }
 	serialize_to_file(config, './data/config.lua')
 	print ('saved config into conf.lua')
